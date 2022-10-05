e9c405afb119c27f71d714a9c79df334c03e2fcf
nisi0929 <kengo279@icloud.com>
2022-10-05 18:04:10 +0900

    add router

--- router.ts
+++ router.ts
@@ -5,3 +5,8 @@ router.get('/', (ctx) => {
   ctx.response.status = Status.OK;
   ctx.response.body = 'todo application with deno';
 });
+
+router.get('/todo', (ctx) => {
+  ctx.response.status = Status.OK;
+  ctx.response.body = 'get /todo dayo';
+});
